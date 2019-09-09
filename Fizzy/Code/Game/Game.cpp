082b7091f756642153148da5c26fb7161411a1bf
cugone <cugone@gmail.com>
2019-09-08 19:28:31 -0500

    Initial Commit
    Initial Commit

---
+++ Fizzy/Code/Game/Game.cpp
@@ -0,0 +1,91 @@
+#include "Game/Game.hpp"
+
+#include "Engine/Core/BuildConfig.hpp"
+#include "Engine/Core/DataUtils.hpp"
+#include "Engine/Core/FileUtils.hpp"
+#include "Engine/Core/KerningFont.hpp"
+
+#include "Engine/Math/Vector2.hpp"
+#include "Engine/Math/Vector4.hpp"
+
+#include "Engine/Renderer/AnimatedSprite.hpp"
+#include "Engine/Renderer/SpriteSheet.hpp"
+#include "Engine/Renderer/Texture.hpp"
+
+#include "Game/GameCommon.hpp"
+#include "Game/GameConfig.hpp"
+
+void Game::Initialize() {
+    g_theRenderer->RegisterMaterialsFromFolder(std::string{ "Data/Materials" });
+}
+
+void Game::BeginFrame() {
+    /* DO NOTHING */
+}
+
+void Game::Update(TimeUtils::FPSeconds deltaSeconds) {
+    if(g_theInputSystem->WasKeyJustPressed(KeyCode::Esc)) {
+        g_theApp->SetIsQuitting(true);
+        return;
+    }
+    Camera2D& base_camera = _ui_camera;
+    HandleDebugInput(base_camera);
+    HandlePlayerInput(base_camera);
+    base_camera.Update(deltaSeconds);
+}
+
+void Game::Render() const {
+    g_theRenderer->ResetModelViewProjection();
+    g_theRenderer->SetRenderTargetsToBackBuffer();
+    g_theRenderer->ClearDepthStencilBuffer();
+
+    g_theRenderer->ClearColor(Rgba::Black);
+
+    g_theRenderer->SetViewportAsPercent();
+
+    //2D View / HUD
+    const float ui_view_height = GRAPHICS_OPTION_WINDOW_HEIGHT;
+    const float ui_view_width = ui_view_height * _ui_camera.GetAspectRatio();
+    const auto ui_view_extents = Vector2{ ui_view_width, ui_view_height };
+    const auto ui_view_half_extents = ui_view_extents * 0.5f;
+    auto ui_leftBottom = Vector2{ -ui_view_half_extents.x, ui_view_half_extents.y };
+    auto ui_rightTop = Vector2{ ui_view_half_extents.x, -ui_view_half_extents.y };
+    auto ui_nearFar = Vector2{ 0.0f, 1.0f };
+    auto ui_cam_pos = ui_view_half_extents;
+    _ui_camera.position = ui_cam_pos;
+    _ui_camera.orientation_degrees = 0.0f;
+    _ui_camera.SetupView(ui_leftBottom, ui_rightTop, ui_nearFar, MathUtils::M_16_BY_9_RATIO);
+    g_theRenderer->SetCamera(_ui_camera);
+
+}
+
+void Game::EndFrame() {
+    /* DO NOTHING */
+}
+
+void Game::HandlePlayerInput(Camera2D& /*base_camera*/) {
+    /* DO NOTHING */
+}
+
+void Game::HandleDebugInput(Camera2D& base_camera) {
+    HandleDebugKeyboardInput(base_camera);
+    HandleDebugMouseInput(base_camera);
+}
+
+void Game::HandleDebugKeyboardInput(Camera2D& /*base_camera*/) {
+    if(g_theUISystem->GetIO().WantCaptureKeyboard) {
+        return;
+    }
+    if(g_theInputSystem->WasKeyJustPressed(KeyCode::F1)) {
+        _show_debug_window = !_show_debug_window;
+    }
+    if(g_theInputSystem->WasKeyJustPressed(KeyCode::F4)) {
+        g_theUISystem->ToggleImguiDemoWindow();
+    }
+}
+
+void Game::HandleDebugMouseInput(Camera2D& /*base_camera*/) {
+    if(g_theUISystem->GetIO().WantCaptureMouse) {
+        return;
+    }
+}
---
+++ Fizzy/Code/Game/Game.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Engine/Core/TimeUtils.hpp"
+#include "Engine/Renderer/Camera2D.hpp"
+
+#include <memory>
+
+
+class Game {
+public:
+    Game() = default;
+    Game(const Game& other) = default;
+    Game(Game&& other) = default;
+    Game& operator=(const Game& other) = default;
+    Game& operator=(Game&& other) = default;
+    ~Game() = default;
+
+    void Initialize();
+    void BeginFrame();
+    void Update(TimeUtils::FPSeconds deltaSeconds);
+    void Render() const;
+    void EndFrame();
+
+protected:
+private:
+    void HandleDebugInput(Camera2D& base_camera);
+    void HandleDebugKeyboardInput(Camera2D& base_camera);
+    void HandleDebugMouseInput(Camera2D& base_camera);
+
+    void HandlePlayerInput(Camera2D& base_camera);
+
+    mutable Camera2D _ui_camera{};
+    float _cam_speed = 1.0f;
+    float _max_shake_angle = 0.0f;
+    float _max_shake_x = 0.0f;
+    float _max_shake_y = 0.0f;
+    bool _show_debug_window = false;
+
+};
+
