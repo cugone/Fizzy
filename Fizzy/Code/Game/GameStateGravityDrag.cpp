d81c1bba46a19f4a827fcb77e3b2d8201af3fd32
cugone <cugone@gmail.com>
2020-09-25 10:56:47 -0500

    Increased impulse force magnitude to 150 from 100 for better "feel"

--- Fizzy/Code/Game/GameStateGravityDrag.cpp
+++ Fizzy/Code/Game/GameStateGravityDrag.cpp
@@ -214,7 +214,7 @@ void GameStateGravityDrag::Debug_ApplyImpulseAtMouseCoords() noexcept {
     const auto p = g_theInputSystem->GetMouseCoords();
     const auto point_on_body = MathUtils::CalcClosestPoint(p, *_activeBody->GetCollider());
     const auto direction = (point_on_body - p).GetNormalize();
-    _activeBody->ApplyImpulse(direction * 100.0f);
+    _activeBody->ApplyImpulse(direction * 150.0f);
 }
 
 void GameStateGravityDrag::ShowDebugWindow() {
